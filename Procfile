bc6c23b470d45ec12fbf413d74c4f90003ab5445
Rajkumar_Pawar <rajm.pawar7038@gmail.com>
2022-12-03 21:03:28 +0530

    railway

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn youdown.wsgi --log-file -
---
+++ youdown/static/javaScript/app.js
@@ -0,0 +1,12 @@
+function Menu(e) {
+  let list = document.querySelector("ul");
+  e.name === "menu"
+    ? ((e.name = "close"),
+      list.classList.add("top-[80px]"),
+      list.classList.add("opacity-100"))
+    : ((e.name = "menu"),
+      list.classList.remove("top-[80px]"),
+      list.classList.remove("opacity-100"));
+}
+
+
