d1bb00b72b3634c292bbfa7af5a3d3ed902a3abd
Rajkumar_Pawar <rajm.pawar7038@gmail.com>
2022-12-03 16:09:51 +0530

    sdkfh

--- youdown/settings.py
+++ youdown/settings.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR,'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
