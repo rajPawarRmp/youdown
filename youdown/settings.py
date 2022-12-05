415538e388f040e7754aac304a78418d8f706125
Rajkumar_Pawar <rajm.pawar7038@gmail.com>
2022-12-05 17:02:22 +0530

    small changes

--- youdown/settings.py
+++ youdown/settings.py
@@ -127,8 +127,8 @@ STATIC_URL = 'static/'
 # ]
 # STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
