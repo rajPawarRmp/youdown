bc632378b0e8bff577c774dc81bee6d01e79c849
Rajkumar_Pawar <rajm.pawar7038@gmail.com>
2022-12-05 16:29:30 +0530

    small changes

--- myapp/views.py
+++ myapp/views.py
@@ -18,21 +18,24 @@ def done(request):
 
 
 def download(request):
-		global url
-		url = request.GET.get('myurl')
-		resolutions = []
-		if request.method == 'GET' and url is not None:
-			obj = YouTube(url)
-			thumb = obj.thumbnail_url
-			title = obj.title
-			streamAll = obj.streams.all()
-			for i in streamAll:
-				resolutions.append(i.resolution)
-			resolutions = list(dict.fromkeys(resolutions))
-			resolutions.remove(None)
-			# print(resolutions, thumb)
-			return render(request, "download.html", {'res': resolutions, 'thum': thumb, 'title': title})
-		return redirect('/')
+		try:
+			global url
+			url = request.GET.get('myurl')
+			resolutions = []
+			if request.method == 'GET' and url is not None:
+				obj = YouTube(url)
+				thumb = obj.thumbnail_url
+				title = obj.title
+				streamAll = obj.streams.all()
+				for i in streamAll:
+					resolutions.append(i.resolution)
+				resolutions = list(dict.fromkeys(resolutions))
+				resolutions.remove(None)
+				# print(resolutions, thumb)
+				return render(request, "download.html", {'res': resolutions, 'thum': thumb, 'title': title})
+			return redirect('/')
+		except Exception as e:
+			raise e	
 
 
 # def downloaded(request,resolution):
@@ -47,33 +50,36 @@ def download(request):
 # 		return render(request, "done.html")
 
 def downloaded(request,resolution):
-   
-		yt=YouTube(url)
-		
-		time = datetime.now().strftime("%H:%M")
-		title=yt.title
-		stream=yt.streams.filter(res=resolution).first()
-		data=stream.download(skip_existing=False)
+			try:	
+				yt=YouTube(url)
+				
+				time = datetime.now().strftime("%H:%M")
+				title=yt.title
+				stream=yt.streams.filter(res=resolution).first()
+				data=stream.download(skip_existing=False)
 
-		path=os.path.normpath(data)
-		
-		with open(path,'rb') as f:
-			byteData=f.read()
-		os.remove(data)
-		return FileResponse(BytesIO(byteData),filename=title+' '+resolution+' ' +time+' youDown.mp4',as_attachment=True,content_type='video/mp4')
-
+				path=os.path.normpath(data)
+				
+				with open(path,'rb') as f:
+					byteData=f.read()
+				os.remove(data)
+				return FileResponse(BytesIO(byteData),filename=title+' '+resolution+' ' +time+' youDown.mp4',as_attachment=True,content_type='video/mp4')
+			except Exception as e:
+				raise e	
 
 def downloaded_audio(request):
-    
-		yt=YouTube(url)
-		
-		time = datetime.now().strftime("%H:%M")
-		title=yt.title
-		stream=yt.streams.filter(only_audio=True).first()
-		data=stream.download(skip_existing=False)
-		path=os.path.normpath(data)
-		with open(path,'rb') as f:
-			byteData=f.read()
-		os.remove(data)
-		return FileResponse(BytesIO(byteData),filename=title +' '+time+' youDown.mp3',as_attachment=True,content_type='audio/mpeg')
+		try:
+			yt=YouTube(url)
+			
+			time = datetime.now().strftime("%H:%M")
+			title=yt.title
+			stream=yt.streams.filter(only_audio=True).first()
+			data=stream.download(skip_existing=False)
+			path=os.path.normpath(data)
+			with open(path,'rb') as f:
+				byteData=f.read()
+			os.remove(data)
+			return FileResponse(BytesIO(byteData),filename=title +' '+time+' youDown.mp3',as_attachment=True,content_type='audio/mpeg')
+		except Exception as e:
+			raise e		
 		# return redirect (request, "done.html")
