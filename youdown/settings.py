1dbdaacaa72d573cf6cdf27f28a77f79aacb004c
Rajkumar_Pawar <rajm.pawar7038@gmail.com>
2022-12-15 23:28:27 +0530

    changes

--- youdown/settings.py
+++ youdown/settings.py
@@ -26,7 +26,7 @@ STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 SECRET_KEY = 'django-insecure-k2@o*o_zpc*&s22=xm5k#i_6u@w)6v(9hb1st_e0n+69l0h)o+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
